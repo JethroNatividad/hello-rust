bcdc916d057a44476d223e357285c1c9c006e650
JethroNatividad <natividad.jet@gmail.com>
2023-12-08 09:18:44 +0800

    get data from input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+// create a program that prompts for your name and prints a greeting using your name.
+
+// name
+// greeting
+// prompt
+// print
+
+// Inputs: name
+// Processes: greeting
+// Outputs: greeting
+
+// Test
+// Inputs: name = Bob
+// Expected Outputs: Hello, Bob, nice to meet you!
+
+// Inputs: name = Alice
+// Expected Outputs: Hello, Alice, nice to meet you!
+
+fn greet(name : &str) -> String {
+    return format!("Hello, {name}, nice to meet you!", name = name.trim());
+}
+
+fn main() -> io::Result<()> {
+    let mut name = String::new();
+    io::stdin().read_line(&mut name)?;
+    let greeting = greet(&name);
+    println!("{greeting}");
+    Ok(())
 }
